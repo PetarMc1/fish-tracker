eee3b43a2f9555e8f67f0e9d09aef6a3cf36f0db
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2025-06-26 16:07:56 +0300

    test(backend): make cache time less

--- backend/src/get/getUserFish.js
+++ backend/src/get/getUserFish.js
@@ -25,7 +25,7 @@ function mapRarity(rarity) {
 
 async function getUserFish(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Cache-Control", "public, s-maxage=20, stale-while-revalidate=60");
+  res.setHeader("Cache-Control", "public, s-maxage=10, stale-while-revalidate=5");
 
   if (req.method !== "GET") {
     res.writeHead(405, { "Content-Type": "application/json" });
