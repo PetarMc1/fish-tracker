3ab540a26ce486be3379b537553c62f18b7f8713
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2025-12-22 21:30:01 +0200

    feat(frontend): conditionally render Navbar on non-admin pages

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Navbar from "@/components/NavBar";
 import "@/styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isAdminPage = router.pathname === '/admin';
+
   return (
     <>
-      <Navbar />
+      {!isAdminPage && <Navbar />}
       <Component {...pageProps}/>
     </>
   );
